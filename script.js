fcae54cdefd7d8bcdffdab80887b474001218883
prithvirajbarad <112259618+prithvirajbarad@users.noreply.github.com>
2023-10-23 19:08:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let browserName = navigator.appName;
+let version = navigator.appVersion;
+document.getElementById("browser-info").innerHTML = "You are using " + browserName + " version " + version;
